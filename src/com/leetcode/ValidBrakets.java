f2937845218dce65070eb42f133e4248124617b2
JiangPeng <1179191373@qq.com>
2019-10-10 23:19:29 +0800

    amend something

---
+++ src/com/leetcode/ValidBrakets.java
@@ -0,0 +1,43 @@
+package com.leetcode;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Stack;
+
+/**
+ * @program: mysql1
+ * @description: 检测是否为有效的括号
+ * @author: Mr.Jiang
+ * @create: 2019-10-10 22:53
+ **/
+public class ValidBrakets {
+    public static void main(String[] args) {
+    }
+}
+
+class ValidBraketsSolution{
+    Map<Character,Character> mapping=new HashMap<>();
+    public ValidBraketsSolution(){
+        this.mapping.put(')','(');
+        this.mapping.put(']','[');
+        this.mapping.put('}','{');
+    }
+    public Boolean isValid(String s){
+        Stack<Character> stack=new Stack<>();
+
+        for (int i=0;i<s.length();i++){
+            char l=s.charAt(i);
+            if (this.mapping.containsKey(l)){
+                char element=stack.isEmpty()? '#':stack.pop();
+                if (this.mapping.get(l)!=element){
+                    return false;
+                }
+            }else {
+                stack.push(l);
+            }
+        }
+        return stack.isEmpty();
+    }
+}
+
+    
