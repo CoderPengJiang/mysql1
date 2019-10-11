32593cd0741fb8c8fd567c683d6b5195686d7ca4
JiangPeng <1179191373@qq.com>
2019-10-11 21:06:12 +0800

    update

--- src/com/leetcode/ValidBrakets.java
+++ src/com/leetcode/ValidBrakets.java
@@ -12,27 +12,34 @@ import java.util.Stack;
  **/
 public class ValidBrakets {
     public static void main(String[] args) {
+        String s="{{{((()))}}}";
+        ValidBraketsSolution vbs=new ValidBraketsSolution();
+        Boolean boolean1=vbs.isValid(s);
+        System.out.println(boolean1);
     }
 }
 
-class ValidBraketsSolution{
-    Map<Character,Character> mapping=new HashMap<>();
-    public ValidBraketsSolution(){
-        this.mapping.put(')','(');
-        this.mapping.put(']','[');
-        this.mapping.put('}','{');
-    }
-    public Boolean isValid(String s){
-        Stack<Character> stack=new Stack<>();
 
-        for (int i=0;i<s.length();i++){
-            char l=s.charAt(i);
-            if (this.mapping.containsKey(l)){
-                char element=stack.isEmpty()? '#':stack.pop();
-                if (this.mapping.get(l)!=element){
+class ValidBraketsSolution {
+    Map<Character, Character> mapping = new HashMap<>();
+
+    public ValidBraketsSolution() {
+        this.mapping.put(')', '(');
+        this.mapping.put(']', '[');
+        this.mapping.put('}', '{');
+    }
+
+    public Boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            char l = s.charAt(i);
+            if (this.mapping.containsKey(l)) {
+                char element = stack.isEmpty() ? '#' : stack.pop();
+                if (this.mapping.get(l) != element) {
                     return false;
                 }
-            }else {
+            } else {
                 stack.push(l);
             }
         }
