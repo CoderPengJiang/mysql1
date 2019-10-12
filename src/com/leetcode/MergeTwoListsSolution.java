b1ecadbcf02ad5f036e13204604dcece91407cdf
JiangPeng <1179191373@qq.com>
2019-10-12 09:04:04 +0800

    amend

--- src/com/leetcode/MergeTwoListsSolution.java
+++ src/com/leetcode/MergeTwoListsSolution.java
@@ -1,5 +1,7 @@
 package com.leetcode;
 
+import javax.xml.soap.Node;
+
 /**
  * @program: mysql1
  * @description: 关于合并两个有序链表的说法
@@ -8,45 +10,109 @@ package com.leetcode;
  **/
 public class MergeTwoListsSolution {
     public static void main(String[] args) {
-        ListNode s1 = new ListNode(1);
-        ListNode s2 = new ListNode(2);
-        ListNode s3 = new ListNode(5);
-        ListNode s4 = new ListNode(6);
-        ListNode s5 = new ListNode(3);
-        ListNode s6 = new ListNode(4);
-        ListNode s7 = new ListNode(7);
-        ListNode s8 = new ListNode(9);
-        List l3=new List();
-        List l4=new List();
-        l3.addListNode(s1);
-        l3.addListNode(s2);
-        l3.addListNode(s3);
-        l3.addListNode(s4);
-        l4.addListNode(s5);
-        l4.addListNode(s6);
-        l4.addListNode(s7);
-        l4.addListNode(s8);
-        MergeSolution ms=new MergeSolution();
-        ms.mergeTwoLists(l3,l4).showListNode();
+        NodeL node = new NodeL(1);
+        NodeL node1 = new NodeL(2);
+        NodeL node2 = new NodeL(3);
+        NodeL node3 = new NodeL(4);
+        node.next = node1;
+        node1.next = node2;
+        node2.next = node3;
+        NodeL node_ = new NodeL(5);
+        NodeL node_1 = new NodeL(6);
+        NodeL node_2 = new NodeL(7);
+        NodeL node_3 = new NodeL(9);
+        node_.next = node_1;
+        node_1.next = node_2;
+        node_2.next = node_3;
+        MergeSolution ms = new MergeSolution();
+        ms.showMergeTwoList(node);
+        System.out.println();
+        ms.showMergeTwoList(node_);
+        System.out.println();
+        NodeL nl = ms.mergeTwoLists(node, node_);
+        ms.showMergeTwoList(nl);
+        ms.addValueLast(node,100);
+        System.out.println();
+        ms.showMergeTwoList(node);
     }
 }
 
-class MergeSolution{
-    public List mergeTwoLists(List l1, List l2) {
-        ListNode prehead=new ListNode(-1);
-        ListNode pre=prehead;
-        while (l1!=null&&l2!=null){
-            if (l1.head.next.val<l2.head.next.val){
-                pre.next=l1.head.next;
-                l1.head.next=l1.head.next.next;
-            }else{
-                pre.next=l2.head.next;
-                l2.head.next=l2.head.next.next;
-            }
-            pre=pre.next;
+class MergeSolution {
+    /**
+     * @Description: 使用迭代法进行连个有序链表的连接操作
+     * @Param: [l1, l2]
+     * @return: com.leetcode.NodeL
+     * @Author: Mr.Jiang
+     * @Date: 2019/10/11
+     */
+    public NodeL mergeTwoLists(NodeL l1, NodeL l2) {
+        if (l1 == null) {
+            return l2;
+        } else if (l2 == null) {
+            return l1;
+        } else if (l1.val < l2.val) {
+            l1.next = mergeTwoLists(l1.next, l2);
+            return l1;
+        } else {
+            l2.next = mergeTwoLists(l1, l2.next);
+            return l2;
         }
-        return pre.next=l1.next==null? l2:l1;
+    }
+
+    /**
+     * @Description: 打印输出链表的值
+     * @Param: [head]
+     * @return: void
+     * @Author: Mr.Jiang
+     * @Date: 2019/10/11
+     */
+    public void showMergeTwoList(NodeL head) {
+        if (head == null) {
+            System.out.println("链表为空");
+        }
+        NodeL temp = head;
+        while (true) {
+            if (temp == null) {
+                break;
+            }
+            System.out.print(temp.val + ",");
+            temp = temp.next;
+        }
+    }
+
+    /**
+     * @Description: 在链表的最后插入一个值
+     * @Param: [l, value]
+     * @return: com.leetcode.NodeL
+     * @Author: Mr.Jiang
+     * @Date: 2019/10/11
+     */
+    public NodeL addValueLast(NodeL l, int value) {
+        NodeL head=new NodeL(-1);
+        head.next=l;
+        NodeL addnode=new NodeL(value);
+        addnode=head;
+        while (true) {
+            if (addnode.next == null) {
+                addnode.next=addnode;
+                break;
+            } else {
+               addnode = addnode.next;
+            }
+        }
+        return l;
     }
 }
+
+class NodeL {
+    public int val;
+    NodeL next;
+
+    public NodeL(int val) {
+        this.val = val;
+    }
+}
+
+
 
     
